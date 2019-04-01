a087036a5219411e2ade19b606408f3dc08bbc9e
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2019-04-01 13:14:45 +0545

    Clean example main file.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,55 +1,19 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
-import 'package:installed_apps/installed_apps.dart';
+void main() => runApp(App());
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
+class App extends MaterialApp {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget get home => HomeScreen();
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await InstalledApps.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Installed Apps Example")),
+      body: Center(
+        child: Text('Running on: A'),
       ),
     );
   }
